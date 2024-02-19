f6425e4600c9866a7827b08df12dac27686f13be
Sayantan Bar <sayantan22.main@gmail.com>
2024-02-19 09:45:07 +0530

    register form implementation complete

---
+++ actions/register.ts
@@ -0,0 +1,13 @@
+"use server";
+import * as z from "zod";
+import { RegisterSchema } from "@/schemas";
+
+export const register = async (value: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(value);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+  console.log(value);
+  return { success: "Email sent!" };
+};
