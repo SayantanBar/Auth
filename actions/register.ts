3aba340d2c1860a20ad65d47fdca6907b917e3ca
Sayantan Bar <sayantan22.main@gmail.com>
2024-02-19 12:34:46 +0530

    implement create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,13 +1,29 @@
 "use server";
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 import { RegisterSchema } from "@/schemas";
-
+import db from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 export const register = async (value: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(value);
 
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
-  console.log(value);
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  // TODO: Send verification token email
+  return { success: "User created!" };
 };
